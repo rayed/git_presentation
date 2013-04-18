d8e158092c0f70b9dac4dba47891db785e1eabb0
Rayed Alrashed <rayed@rayed.com>
2013-04-18 10:32:18 +0300

    Remove empty lines in Git basic section

--- 010-git-basics.php
+++ 010-git-basics.php
@@ -6,16 +6,14 @@
 <section>
 <h2>Local Repository</h2>
 <p>Life without Git</p>
-<pre class="fragment"><code>
-$ ls -l
+<pre class="fragment"><code>$ ls -l
 img/
 css/
 js/
 index.html
 </code></pre>
 
-<pre class="fragment"><code>
-$ git status
+<pre class="fragment"><code>$ git status
 fatal: Not a git repository (or any of the parent directories): .git
 </code></pre>
 </section>
@@ -24,12 +22,10 @@ fatal: Not a git repository (or any of the parent directories): .git
 <section>
 <h2>Local Repository</h2>
 <p>Create Local Repository</p>
-<pre class="fragment"><code>
-$ git init .
+<pre class="fragment"><code>$ git init .
 </code></pre>
 
-<pre class="fragment"><code>
-$ ls -la
+<pre class="fragment"><code>$ ls -la
 .git  &lt;=======  Git Local Repo
 img/
 css/
@@ -42,8 +38,7 @@ index.html
 <section>
 <h2>Local Repository</h2>
 <p>Untracked</p>
-<pre class="fragment"><code>
-$ git status
+<pre class="fragment"><code>$ git status
 # On branch master
 #
 # Initial commit
@@ -63,12 +58,10 @@ nothing added to commit but untracked files present (use "git add" to track)
 <section>
 <h2>Local Repository</h2>
 <p>Track and Stage</p>
-<pre class="fragment"><code>
-$ git add .  &lt;====== "." everything in current dir
+<pre class="fragment"><code>$ git add .  &lt;====== "." everything in current dir
 </code></pre>
 
-<pre class="fragment"><code>
-$ git status
+<pre class="fragment"><code>$ git status
 # On branch master
 #
 # Initial commit
@@ -88,12 +81,10 @@ $ git status
 <section>
 <h2>Local Repository</h2>
 <p>Commit!!</p>
-<pre class="fragment"><code>
-$ git commit -m "My First Commit"
+<pre class="fragment"><code>$ git commit -m "My First Commit"
 </code></pre>
 
-<pre class="fragment"><code>
-$ git status
+<pre class="fragment"><code>$ git status
 # On branch master
 nothing to commit, working directory clean
 </code></pre>
@@ -103,12 +94,10 @@ nothing to commit, working directory clean
 <section>
 <h2>Local Repository</h2>
 <p>Change Code</p>
-<pre class="fragment"><code>
-$ edit index.html  &lt;============== CHANGE FILE
+<pre class="fragment"><code>$ edit index.html  &lt;============== CHANGE FILE
 $ edit about.html  &lt;============== NEW FILE
 </code></pre>
-<pre class="fragment"><code>
-$ git status
+<pre class="fragment"><code>$ git status
 # Changes not staged for commit:
 #	modified:   index.html
 #
@@ -124,14 +113,11 @@ no changes added to commit (use "git add" and/or "git commit -a")
 <section>
 <h2>Local Repository</h2>
 <p>Stage <span class="fragment">and Track</span></p>
-<pre class="fragment"><code>
-$ git add index.html  &lt; ============== Stage
+<pre class="fragment"><code>$ git add index.html  &lt; ============== Stage
 </code></pre>
-<pre class="fragment"><code>
-$ git add about.html  &lt; ============== Track and Stage
+<pre class="fragment"><code>$ git add about.html  &lt; ============== Track and Stage
 </code></pre>
-<pre class="fragment"><code>
-$ git status
+<pre class="fragment"><code>$ git status
 # Changes to be committed:
 #	new file:   about.html
 #	modified:   index.html
@@ -143,11 +129,9 @@ $ git status
 <section>
 <h2>Local Repository</h2>
 <p>My Second Commit</p>
-<pre class="fragment"><code>
-$ git commit -m "Added About page"
+<pre class="fragment"><code>$ git commit -m "Added About page"
 </code></pre>
-<pre class="fragment"><code>
-$ git status 
+<pre class="fragment"><code>$ git status 
 # On branch master
 nothing to commit, working directory clean
 </code></pre>
@@ -184,16 +168,14 @@ Commit when project reach working state
 $ git log
 </code></pre>
 
-<pre class="fragment"><code>
-commit e78205f5b61fa5ef8f94af7bae5a5783bb8a0625
+<pre class="fragment"><code>commit e78205f5b61fa5ef8f94af7bae5a5783bb8a0625
 Author: Rayed Alrashed &lt;rayed@example.com&gt;
 Date:   Tue Mar 12 23:28:54 2013 +0000
 
 Added About page
 </code></pre>
 
-<pre class="fragment"><code>
-commit 45b30fc4324e116db551845e588c9086aa26a625
+<pre class="fragment"><code>commit 45b30fc4324e116db551845e588c9086aa26a625
 Author: Rayed Alrashed &lt;rayed@example.com&gt;
 Date:   Wed Mar 27 06:28:02 2013 +0000
 
@@ -212,12 +194,10 @@ My first commit
 
 <section>
 <h2>Travel to the Past</h2>
-<pre class="fragment"><code>
-$ git checkout 45b30fc4324e116db551845e588c9086aa26a625
+<pre class="fragment"><code>$ git checkout 45b30fc4324e116db551845e588c9086aa26a625
 </code></pre>
 
-<pre class="fragment"><code>
-$ ls -l
+<pre class="fragment"><code>$ ls -l
 .git
 img/
 css/
@@ -234,12 +214,10 @@ index.html  &gt; ==== Our OLD index
 
 <section>
 <h2>Return to the Present</h2>
-<pre class="fragment"><code>
-$ git checkout master
+<pre class="fragment"><code>$ git checkout master
 </code></pre>
 
-<pre class="fragment"><code>
-$ ls -l
+<pre class="fragment"><code>$ ls -l
 .git
 img/
 css/
@@ -253,11 +231,9 @@ about.html  &gt; ==== about.html is Back
 <section>
 <h2>What Changed</h2>
 <p>Show code changes between 2 commits</p>
-<pre class="fragment"><code>
-$ git diff 45b30fc4324e116db551845e588c9086aa26a625 
+<pre class="fragment"><code>$ git diff 45b30fc4324e116db551845e588c9086aa26a625 
 </code></pre>
-<pre class="fragment"><code>
---- a/about.html
+<pre class="fragment"><code>--- a/about.html
 +++ b/about.html
 @@ -1 +1 @@
 -&lt;h1&gt;About Us&lt;/h1&gt;
